d50eddd0d373f3bdd40821dd776914f5ec03ee86
Samuel R Souza <127272947+samuelsouzzza@users.noreply.github.com>
2023-08-08 17:14:44 -0300

    Create 8kyu_invertValues.ts

---
+++ 8kyu_invertValues.ts
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+function invert(array: number[]): number[] {
+ return array.map(n => {
+     return n >= 0 ? -n : n - (n + n);
+  });
+}
